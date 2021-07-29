03f1b56061bfede6d7cd85dcb9f9908c3c6a0467
Anis Haddouche <66164454+AnisHaddouche@users.noreply.github.com>
2021-07-30 00:42:41 +0200

    sketch 1

---
+++ source/describe.py
@@ -0,0 +1,15 @@
+"""
+The main of this scripte is the following.
+We assume that we have the database "TVAR" with the tables : avdertisements, descriptors, apparitions
+
+Function 1 :
+input = Folder --> output = the table "avdertisements"
+- From the folder advertisements read each ads and extracte the first and the last frame
+- For each ads create the liste [id, name, first_frame, last_frame, duration]
+- Write in to the table "avdertisements"
+
+Function 2 :
+input = output of Function 1 --> output = the table " descriptors"
+- For each line in the table "avdertisements" create [id, name, first_frame_descriptor, last_frame_descriptor, duration]
+- Write on the table "descriptors"
+"""
