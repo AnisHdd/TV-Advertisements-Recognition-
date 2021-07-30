647d027c49aff496905fc0bcf84589a1523cf8a3
Anis Haddouche <66164454+AnisHaddouche@users.noreply.github.com>
2021-07-30 18:38:12 +0200

    v1 adel anis

--- source/database_sql.py
+++ source/database_sql.py
@@ -1,16 +1,5 @@
 import mysql.connector
 """ Create  mysql database "TvAdsReco" """
-# mydb = mysql.connector.connect(
-#     host="127.0.0.1",
-#     user="root",
-#     password=""
-# )
-# mycursor = mydb.cursor()
-# mycursor.execute("CREATE DATABASE TvAdsReco")
-# mycursor.execute("SHOW DATABASES")
-#
-# for x in mycursor:
-#   print(x)
 """ Connecte to the database "TvAdsReco" """
 mydb = mysql.connector.connect(
     host="127.0.0.1",
@@ -20,17 +9,63 @@ mydb = mysql.connector.connect(
 )
 mycursor = mydb.cursor()
 
-"""Create the table "advertisements" """
-mycursor.execute("CREATE TABLE IF NOT EXISTS advertisements(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), path VARCHAR(255))")
-mycursor.execute("CREATE TABLE IF NOT EXISTS descriptors(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), first_frame VARCHAR(255),last_frame VARCHAR(255) )")
+"""
+Create the table "advertisements" 
+Ex : [id*(int20), id_brand(int20), name(VARCHAR(225)), path(VARCHAR(225)) ,ffdes(json), lfdes(json), duration(time), date(timestamp)]
+    =[1, 1, mobilis1,...,...,...,...]
+     [2, 1, mobilis2,...,...,...,...] 
+"""
+mycursor.execute("CREATE TABLE IF NOT EXISTS advertisements(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), "
+                 "path VARCHAR(255), ff_descriptor JSON, lf_descriptor JSON, duration TIME, created_at TIMESTAMP "
+                 "DEFAULT CURRENT_TIMESTAMP)")
+
+""""
+Create the table brands 
+Ex : [id, name, created_at] = [1 mobilis] 
+"""
+mycursor.execute("CREATE TABLE IF NOT EXISTS brands(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), created_at "
+                 "TIMESTAMP DEFAULT CURRENT_TIMESTAMP)")
+
+""""
+Create the table channels 
+Ex : [id, name, url] 
+"""
+mycursor.execute("CREATE TABLE IF NOT EXISTS channels(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), "
+                 "url VARCHAR(255))")
+
+""""
+Create the table apparitions 
+Ex : [id, id_advetisements, id_channel, time_start, time_end, date]
+"""
+mycursor.execute("CREATE TABLE IF NOT EXISTS apparitions(id INT AUTO_INCREMENT PRIMARY KEY, id_advetisements INT, "
+                 "id_channel INT, time_start TIME, time_end TIME, appeared_at TIMESTAMP)")
 
 
-""" Create an auto incremeant colonne for each table"""
-#mycursor.execute("ALTER TABLE advertisements ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
-#mycursor.execute("ALTER TABLE descriptors ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
-#mycursor.execute("ALTER TABLE apparitions ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
+class database(object):
+    def __init__(self, host, user, password, database_name):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database_name = database_name
+        self.mydb = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database_name
+        )
+        self.mycursor = self.mydb.cursor()
 
-"""Create columns of the table advertisements  """
-mycursor.execute("SHOW TABLES")
-for x in mycursor:
-  print(x)
+
+
+
+#####
+# db = database("127.0.0.1", "root", "", "TvAdsReco")
+# db.mycursor.execute("SHOW TABLES")
+# for x in db.mycursor:
+#   print(x)
+# Ex : [id, name, url]
+# mycursor = mydb.cursor()
+# sql = "INSERT INTO channels (name, url) VALUES (%s, %s)"
+# val = ("elbilad", "https://www.nikemok.zebi")
+# mydb.commit()
+# print(mycursor.rowcount, "record inserted.")
--- source/tvar.py
+++ source/tvar.py
@@ -1,12 +1,40 @@
 """ """
+from database_sql import database
 
 
-class tvar():
-    def __init__(self, config):
-        self.config = config
+class tvar(object):
+    def __init__(self):
+        self.db = database("127.0.0.1", "root", "", "TvAdsReco")
 
-       # initialize db
-        db_cls = get_database(config.get("database_type", 'None'))
+        """ add a new ads in channels"""
 
-        self.db = db_cls(**config.get("database", {}))
-        self.db.setup()
+    def insert_advertisements(self,name,path,ff_descriptor,lf_descriptor,duration):
+        self.db.mycursor.execute("INSERT INTO advertisements (name,path,ff_descriptor,lf_descriptor,duration) VALUES (%s, %s, %s, %s, %s)"
+                                 ,(name,path,ff_descriptor,lf_descriptor,duration))
+        self.db.commit()
+        print(self.db.mycursor.rowcount, "record inserted.")
+
+
+
+     def describe(self):
+        """ entree: path vers le videos --- > remplir la base de donnee advertisements """
+        self.db.mycursor.execute("SHOW TABLES")
+        for x in self.db.mycursor:
+            print(x)
+        pass
+
+    def recognize(self):
+        """entree video --- > chercher dans la bdd avdertisement la publicite et remplir la table apparitions """
+        pass
+
+
+
+
+
+
+
+#
+# detecteur = tvar()
+# detecteur.db.mycursor.execute("SHOW TABLES")
+# for x in detecteur.db.mycursor:
+#     print(x)
