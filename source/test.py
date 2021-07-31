02f1d5ec27d5c57a7fd65b674af648f25f9c36c6
Anis Haddouche <66164454+AnisHaddouche@users.noreply.github.com>
2021-07-31 11:34:41 +0200

    bdd
    comment stocker les descripteur dans la bdd ?

--- source/test.py
+++ source/test.py
@@ -1,18 +1,23 @@
 import cv2
 import ntpath
 import os
-
+import tvar
 import numpy as np
 
+orb = cv2.ORB_create(nfeatures=100)
+bf = cv2.BFMatcher(cv2.NORM_HAMMING2)
+_, des_ST = orb.detectAndCompute(Data[name].Start, None)
+_, des_EN = orb.detectAndCompute(Data[name].End, None)
 
 def extract_frames_folder(path):
     myList_Adv = os.listdir(path)
     if '.DS_Store' in myList_Adv:
         myList_Adv.remove('.DS_Store')
-    number=np.size(myList_Adv)
+    number = np.size(myList_Adv)
     print(number, type(myList_Adv))
-    for i in range(0,number):
+    for i in range(0, number):
         print(i)
 
 
-extract_frames_folder("/Users/macbookpro/Library/Mobile Documents/com~apple~CloudDocs/PycharmProjects/OpenCV/Commercial-detection /src/videos")
+extract_frames_folder(
+    "/Users/macbookpro/Library/Mobile Documents/com~apple~CloudDocs/PycharmProjects/OpenCV/Commercial-detection /src/videos")
--- source/tvar.py
+++ source/tvar.py
@@ -4,46 +4,52 @@ import ntpath
 import os
 import numpy as np
 from database_sql import database
+from sys import getsizeof
+import json
 
 
 class tvar(object):
     def __init__(self):
         self.db = database("127.0.0.1", "root", "", "TvAdsReco")
 
-
-    def insert_advertisements(self,name,path,ff_descriptor,lf_descriptor,duration):
+    def insert_advertisement(self, name, path, ff_descriptor, lf_descriptor, duration):
         """ add a new ads in channels"""
         self.db.mycursor.execute("INSERT INTO advertisements (name,path,ff_descriptor,lf_descriptor,duration) VALUES "
                                  "(%s, %s, %s, %s, %s) "
-                                 ,(name,path,ff_descriptor,lf_descriptor,duration))
+                                 , (name, path, ff_descriptor, lf_descriptor, duration))
         self.db.commit()
         print(self.db.mycursor.rowcount, "record inserted.")
 
-
-    def extract_frames_file(self,path_file):
+    def extract_frames_file(self, path_file):
         """ Extract the first and the last frame from a given ads path"""
+        orb = cv2.ORB_create(nfeatures=100)
+        name = ntpath.basename(path_file)
         cap = cv2.VideoCapture(path_file)
         cap.set(1, cap.get(cv2.CAP_PROP_FRAME_COUNT) - 1)
         _, last_frame = cap.read()
+        last_frame = cv2.cvtColor(last_frame, cv2.COLOR_BGR2GRAY)
+        _, des_last_frame = orb.detectAndCompute(last_frame, None)
         cap.set(1, 1)
         _, first_frame = cap.read()
-        name = ntpath.basename(path_file)
-        # cv2.imshow("as", first_frame)
+        first_frame = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
+        _, des_first_frame = orb.detectAndCompute(first_frame, None)
+        # cv2.imshow("as", last_frame)
         # while True:
         #     ch = 0xFF & cv2.waitKey(1)  # Wait for a second
         #     if ch == 27:
         #         break
-        return  first_frame, last_frame, name
+        return des_first_frame, des_last_frame, name
 
-    def extract_frames_folder(self,path):
-        myList_Adv = os.listdir(path)
-        if '.DS_Store' in myList_Adv:
-            myList_Adv.remove('.DS_Store')
-        for id in range(0,np.size(myList_Adv)):
-            first_frame, last_frame, name= self.extract_frames_file(path+"/"+str(myList_Adv[id]))
-            #cv2.imwrite(str(name)+"_"+"first_frame.jpeg",first_frame)
-            #cv2.imwrite(str(name)+"_"+"last_frame.jpeg",last_frame)
-            # print(str(name)+"_"+"first_frame.jpeg",first_frame)
+    def extract_frames_folder(self, path):
+        list_ads = os.listdir(path)
+        if '.DS_Store' in list_ads:
+            list_ads.remove('.DS_Store')
+        for i in range(0, np.size(list_ads)):
+            des_first_frame, des_last_frame, name = self.extract_frames_file(path + "/" + str(list_ads[i]))
+            # cv2.imwrite(str(name)+"_"+"first_frame.jpeg",first_frame)
+            # cv2.imwrite(str(name)+"_"+"last_frame.jpeg",last_frame)
+            print(name)
+
 
     def describe(self):
         """ entree: path vers le videos --- > remplir la base de donnee advertisements """
@@ -57,14 +63,19 @@ class tvar(object):
         pass
 
 
-
-
-
-
-
 #
 detecteur = tvar()
-detecteur.extract_frames_folder('/Users/macbookpro/Library/Mobile Documents/com~apple~CloudDocs/PycharmProjects/OpenCV/Commercial-detection /src/videos')
+detecteur.extract_frames_folder('/Users/macbookpro/Library/Mobile '
+                                        'Documents/com~apple~CloudDocs/PycharmProjects/OpenCV/Commercial-detection '
+                                        '/src/videos')
+
+
+# json_str = json.dumps(x.tolist())
+# print(print(json_str),type(json_str) ,getsizeof(json_str))
+# x = np.array(json_str)
+# x.reshape(100)
+# print(print(x),type(x) ,getsizeof(x),np.size(x))
+
 # detecteur.db.mycursor.execute("SHOW TABLES")
 # for x in detecteur.db.mycursor:
 #     print(x)
